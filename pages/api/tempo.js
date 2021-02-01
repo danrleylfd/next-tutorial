0efe4f50a7d37020dbf1978f93cf2b4a6a3f26f6
danrleylfd <danrleylfd@gmail.com>
2021-02-01 13:05:51 -0300

    dopamina

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,10 +2,9 @@ async function Tempo(req,res) {
   const apiSecret = process.env.CONVERTKIT_API_SECRET;
   const dynamicDate = new Date();
   const date = dynamicDate.toGMTString();
-  const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+  const subscribersResponse = await fetch(`https://api.github.com/users/danrleylfd/following`);
   const subscribersResponseJson = await subscribersResponse.json();
-  const subscribers = subscribersResponseJson.total_subscribers; 
-
+  const subscribers = subscribersResponseJson.length; 
   res.setHeader(
     'Cache-Control',
     's-maxage=15, stale-while-revalidate'
