72667de8608c2554fdd58395151a691e4fb9c254
danrleylfd <danrleylfd@gmail.com>
2021-02-01 12:14:23 -0300

    fdpmn

---
+++ components/counter.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function Contador() {
+  const [contador,setContador] = useState(1);
+  function adicionarContador(){
+    setContador(contador+1)
+  }
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>adicionar</button>
+    </div>
+  );
+}
+
+export default Contador;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
-import { useState } from 'react'
 import Link from 'next/link';
+import Contador from '../components/counter';
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Contador />
       <Link href="/about">
         <a>Acessar página sobre</a>
       </Link>
-    </div>
-  );
-}
-
-function Contador() {
-  const [contador,setContador] = useState(1);
-  function adicionarContador(){
-    setContador(contador+1)
-  }
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>adicionar</button>
+      <Contador />
     </div>
   );
 }
