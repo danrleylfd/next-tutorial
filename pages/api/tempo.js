6cfbb97e00307474ffda7a0b94a2db89c3f07525
danrleylfd <danrleylfd@gmail.com>
2021-02-01 11:41:58 -0300

    fdpmn

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,9 @@
-function tempo(req,res) {
+function Tempo(req,res) {
   const dynamicDate = new Date();
-  res.setHeader('Cache-Control', 's-maxage=7, stale-while-revalidate');
+  res.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate');
   res.json({
     date: dynamicDate.toGMTString()
   });
 }
 
-export default tempo;
+export default Tempo;
