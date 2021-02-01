ca333bf57dd2c471dd0b233c4ad2bd2f9da00dc0
danrleylfd <danrleylfd@gmail.com>
2021-02-01 12:51:43 -0300

    fdpmn

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,16 @@
-function Tempo(req,res) {
+async function Tempo(req,res) {
+  const apiSecret = process.env.CONVERTKIT_API_SECRET;
   const dynamicDate = new Date();
-  res.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate');
-  res.json({
-    date: dynamicDate.toGMTString()
-  });
+  const date = dynamicDate.toGMTString();
+  const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+  const subscribersResponseJson = await subscribersResponse.json();
+  const subscribers = subscribersResponseJson.total_subscribers; 
+
+  res.setHeader(
+    'Cache-Control',
+    's-maxage=15, stale-while-revalidate'
+  );
+  res.json({ date, subscribers });
 }
 
 export default Tempo;
